9fa1e5c47bbd0667e7b168bdde8da0176c8d783d
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-04 20:11:56 +0500

    Icons were added

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,7 @@
+class AppIcons {
+  static const String home = 'assets/icons/home.svg';
+  static const String car = 'assets/icons/car.svg';
+  static const String git = 'assets/icons/git.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String person = 'assets/icons/person.svg';
+}
