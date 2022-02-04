4da00ef1c0b6c70cee90d28d1a9ea58c2bb557cd
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-04 16:30:32 +0500

    TextFormFieldWidget was added

--- lib/core/components/exporting_packages.dart
+++ lib/core/components/exporting_packages.dart
@@ -1,4 +1,11 @@
 // Screens
 export 'package:mobileapp/screens/home/home_page.dart';
 
+
+// Widgets
+export 'package:mobileapp/widgets/my_text_form_field.dart';
+
 export 'package:mobileapp/core/components/size_config.dart';
+
+// Constants
+export 'package:mobileapp/core/constants/app_colors.dart';
---
+++ lib/widgets/my_text_form_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:mobileapp/core/components/exporting_packages.dart';
+
+class TextFormFieldWidget extends StatelessWidget {
+  TextEditingController? controller;
+  String? hint;
+  TextInputAction action;
+  TextInputType inputType;
+  bool obscureText;
+  TextFormFieldWidget({
+    Key? key,
+    this.controller,
+    this.hint,
+    this.action = TextInputAction.next,
+    this.inputType = TextInputType.text,
+    this.obscureText = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      textInputAction: action,
+      keyboardType: inputType,
+      obscureText: obscureText,
+      style: _textStyle(AppColors.black),
+      decoration: _buildInputDecoration(),
+    );
+  }
+
+  InputDecoration _buildInputDecoration() {
+    return InputDecoration(
+      hintText: hint,
+      hintStyle: _textStyle(AppColors.grey),
+      border: _outlineInputBorder(),
+      enabledBorder: _outlineInputBorder(),
+      focusedBorder: _outlineInputBorder(),
+      contentPadding: EdgeInsets.all(getWidth(18.0))
+    );
+  }
+
+  TextStyle _textStyle(Color color) {
+    return TextStyle(
+      fontSize: getWidth(16.0),
+      color: color
+    );
+  }
+
+  OutlineInputBorder _outlineInputBorder() => const OutlineInputBorder(
+    borderSide: BorderSide(color: AppColors.grey)
+  );
+}
