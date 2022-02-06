7b4d211bd41a91bce8bd151b51380c991cd0372a
Ramazon-dev <9621681@gmail.com>
2022-02-06 15:47:15 +0500

    togirlandi

--- lib/screens/details/git_details.dart
+++ lib/screens/details/git_details.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
 import 'package:mobileapp/widgets/appbar_origin.dart';
-import 'package:mobileapp/widgets/appbar_widget.dart';
-<<<<<<< HEAD:lib/screens/details/git_details.dart
 import 'package:mobileapp/widgets/contact_widget.dart';
 import 'package:mobileapp/widgets/description_widjet.dart';
 import 'package:mobileapp/widgets/language_button.dart';
-=======
-import 'package:mobileapp/widgets/drawer_widget.dart';
->>>>>>> main:lib/screens/git/git_desciription.dart
 
 class GitDescriptionPage extends StatelessWidget {
   String imageurl;
