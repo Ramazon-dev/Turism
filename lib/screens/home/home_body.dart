4e888f43ed9d7c5af447ea2b0fb8e9cdeb085b91
Ramazon-dev <9621681@gmail.com>
2022-02-06 15:16:58 +0500

    boshqattan togirlandi

--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -1,44 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:mobileapp/screens/auth/sign_in/sign_in_page.dart';
-import 'package:mobileapp/translations/locale_keys.g.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
-import 'package:mobileapp/widgets/appbar_widget.dart';
-import 'package:mobileapp/widgets/drawer_widget.dart';
-import 'package:mobileapp/widgets/elevated_button_widget.dart';
-import 'package:mobileapp/widgets/url_icon_widget.dart';
-import 'package:mobileapp/widgets/url_text_widgte.dart';
+import 'package:mobileapp/translations/locale_keys.g.dart';
+import 'package:mobileapp/widgets/divider_widgets.dart';
+import 'package:mobileapp/widgets/text_widgets.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        UrlTextWidget(
-          url: 'https://github.com/Ramazon-dev/',
-          text: 'GitHub linki',
-        ),
-        UrlIconWidget(
-          icon: SvgPicture.asset(
-            AppIcons.email,
-            fit: BoxFit.scaleDown,
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextWidgetTwo(
+            "Давайте спланируем\nваше следующее приключение",
+            style: TextWidgetTwo.medium(size: 20.0),
           ),
-          url: 'https://t.me/Ramazon1681',
-        ),
-        const Center(
-          child: Text("Home Page"),
-        ),
-        InkWell(
-          onTap: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SignInPage())),
-          child: ElevatedButtonWidget(
-            text: 'button',
+          const dividerWidget(),
+          Container(
+            alignment: Alignment.center,
+            height: getHeight(56.0),
+            color: Colors.blue,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: getWidth(65.0),
+                    height: getHeight(39.0),
+                    color: Colors.greenAccent,
+                  ),
+                );
+              },
+            ),
           ),
-        ),
-      ],
+          TextWidgetTwo(
+            "Ближайшие туры.",
+            style: TextWidgetTwo.medium(size: 18.0),
+          ),
+          Container(
+            height: 200.0,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: getHeight(200.0),
+                      width: 330.0,
+                      decoration: MyDecoration.netImage(
+                          netImage: "https://source.unsplash.com/random/1",
+                          border: 5.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            height: getHeight(22.0),
+                            width: getWidth(51.0),
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
 import 'package:mobileapp/cubit/home_cubit/cubit/home_cubit.dart';
-import 'package:mobileapp/widgets/appbar_origin.dart';
-import 'package:mobileapp/widgets/drawer_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,15 +14,6 @@ class HomeScreen extends StatelessWidget {
         builder: (context, state) {
           HomeCubit cubit = context.watch();
           return Scaffold(
-            appBar: AppBarOrigin(
-              text: 'Appbar Origin',
-              actions: SvgPicture.asset(
-                AppIcons.language,
-                color: AppColors.white,
-              ),
-              actions2: SvgPicture.asset(AppIcons.dollar),
-            ),
-            drawer: const DrawerDiwget(),
             body: _pages()[cubit.currentIndex],
             bottomNavigationBar: BottomNavBarWidget(
               onTap: cubit.onPageChanged,
