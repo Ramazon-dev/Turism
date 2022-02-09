9f93659c08ac0e1cf09850c1445640696052a247
Zuhriddin Yoqubov <zuhriddinyoqub@gmail.com>
2022-02-09 14:24:42 +0500

    comment servis ishlanmalari

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,10 +1,9 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobileapp/services/auth_services.dart';
 
 void main() async {
   test(
-    "User singIn test",
+    "User singIn",
     () async {
       var signIn = await AuthServices.signIn("umida@gmail.com", "Umida7\$");
       expect(signIn, true);
@@ -12,11 +11,14 @@ void main() async {
   );
 
   test(
-    "User singUp test",
+    "User register test",
     () async {
-      var signUp =
-          await AuthServices.signUpWithHttp('fullfsdfsName', 'emafssfil@fmsdf.ur', 'Pass2fdsfs421fas');
-      expect(signUp, true);
+      var result = await AuthServices.register(
+        fullName: 'fullfsdfsName',
+        email: 'emafscxdfscdssfil@fmsdf.ur',
+        password: 'Pass2fdsfs421fas',
+      );
+      expect(result, true);
     },
   );
 }
