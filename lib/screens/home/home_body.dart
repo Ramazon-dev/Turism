dafac15d78b87e8b9aa29958d72874dc23d7d6a9
BahromjonPolat <itjunior.uz@gmail.com>
2022-02-09 12:09:09 +0500

    BigFixes

--- lib/screens/home/home_body.dart
+++ lib/screens/home/home_body.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
-
 import 'package:mobileapp/translations/locale_keys.g.dart';
 import 'package:mobileapp/widgets/appbar_origin.dart';
 
 import 'package:mobileapp/widgets/divider_widgets.dart';
+import 'package:mobileapp/widgets/text_widgets.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({Key? key}) : super(key: key);
@@ -13,106 +13,115 @@ class HomeBody extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            alignment: Alignment.center,
-            height: getHeight(56.0),
-            color: Colors.blue,
-            child: ListView.builder(
-              itemCount: 10,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: getWidth(65.0),
-                    height: getHeight(39.0),
-                    color: Colors.greenAccent,
-                  ),
-                );
-              },
-            ),
-
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          _buildTop(),
+          _buildTextWidgetTwo(),
+          _showNearby(),
           const dividerWidget(),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: getHeight(593.5),
-            child: GridView.builder(
-              itemCount: 9,
-              padding: MyEdgeInsets.symmetric(h: 16.0, v: 25.0),
-              gridDelegate: (SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.77,
-                crossAxisSpacing: getWidth(8.0),
-                mainAxisSpacing: getHeight(20.0),
-              )),
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: MyDecoration.shadow(color: Colors.black12),
-                  height: getHeight(273.0),
-                  width: getWidth(165.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: getHeight(200.0),
-                        width: getWidth(165.0),
-                        decoration: MyDecoration.netImage(
-                          
-                            netImage:
-                                "https://source.unsplash.com/random/index1",
-                            border: 5.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: SvgPicture.asset(
-                                AppIcons.favourite,
-                              ),
-                            ),
-                            
-                          ],
-                        ),
+          _showTur(context),
+        ]),
+      ),
+    );
+  }
 
-          TextWidgetTwo(
-            "Ближайшие туры.",
-            style: TextWidgetTwo.medium(size: 18.0),
-          ),
-          Container(
-            height: 200.0,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: getHeight(200.0),
-                      width: 330.0,
-                      decoration: MyDecoration.netImage(
-                          netImage: "https://source.unsplash.com/random/1",
-                          border: 5.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Container(
-                            height: getHeight(22.0),
-                            width: getWidth(51.0),
-                            color: Colors.yellow,
-                          ),
-                        ],
-
+  GridView _showTur(BuildContext context) {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemCount: 9,
+        padding: MyEdgeInsets.symmetric(h: 16.0, v: 25.0),
+        gridDelegate: (SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.77,
+          crossAxisSpacing: getWidth(8.0),
+          mainAxisSpacing: getHeight(20.0),
+        )),
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: MyDecoration.shadow(color: Colors.black12),
+            height: getHeight(273.0),
+            width: getWidth(165.0),
+            child: Column(children: [
+              Container(
+                height: getHeight(200.0),
+                width: getWidth(165.0),
+                decoration: MyDecoration.netImage(
+                    netImage: "https://source.unsplash.com/random/index1",
+                    border: 5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SvgPicture.asset(
+                        AppIcons.favourite,
                       ),
-                     
-                    ],
-                  ),
-                );
-              },
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          );
+        });
+  }
+
+  TextWidgetTwo _buildTextWidgetTwo() {
+    return TextWidgetTwo(
+      "Ближайшие туры.",
+      style: TextWidgetTwo.medium(size: 18.0),
+    );
+  }
+
+  SizedBox _showNearby() {
+    return SizedBox(
+      height: 200.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: getHeight(200.0),
+                width: 330.0,
+                decoration: MyDecoration.netImage(
+                    netImage: "https://source.unsplash.com/random/1",
+                    border: 5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: getHeight(22.0),
+                      width: getWidth(51.0),
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+              ));
+        },
+      ),
+    );
+  }
+
+  Container _buildTop() {
+    return Container(
+      alignment: Alignment.center,
+      height: getHeight(56.0),
+      color: Colors.blue,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: getWidth(65.0),
+              height: getHeight(39.0),
+              color: Colors.greenAccent,
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
