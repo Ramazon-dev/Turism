b8b7714e57da7aead2bf2aa4625a654a43fe83be
Zuhriddin Yoqubov <zuhriddinyoqub@gmail.com>
2022-02-18 21:29:13 +0500

    taxrirlar

--- lib/services/hotel_service.dart
+++ lib/services/hotel_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:mime/mime.dart';
@@ -9,73 +8,66 @@ class HotelService {
   static String baseUrl = 'https://ucharteam-tourism.herokuapp.com/v1/api';
 
   static Future createNewHotel(Hotel hotel) async {
-    print('create new hotel metodi ishga tushdi');
+    try {
+      var headers = {
+        'token': //await GetStorage().read('token');
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMzIyYjkxNi01MjQ0LTQ5YTItOWY0Ni1jM2E3YTYzNjA0Y2IiLCJpYXQiOjE2NDUwOTUwNzEsImV4cCI6MTY2MjM3NTA3MX0.cX0A_pOKUn7K6iekxocSWK4K5WrtHph_2-WrOXPDyis'
+      };
+      var request = http.MultipartRequest('POST', Uri.parse('$baseUrl/hotel'));
+      request.fields.addAll({
+        'name': hotel.name,
+        'city': hotel.city,
+        'informUz': hotel.informUz,
+        'informRu': hotel.informRu,
+        'informEn': hotel.informEn,
+        'karta': hotel.karta,
+        'tell': hotel.tell.toString(),
+        'categoryId': '1991edea-7d4a-49fb-b627-79b777cf54ae'
+      });
 
-    Future createNewHotel(String filePath, Hotel hotel) async {
-      print("FilePath: $filePath");
-
-      try {
-        var headers = {
-          'token': //await GetStorage().read('token');
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMzIyYjkxNi01MjQ0LTQ5YTItOWY0Ni1jM2E3YTYzNjA0Y2IiLCJpYXQiOjE2NDUwOTUwNzEsImV4cCI6MTY2MjM3NTA3MX0.cX0A_pOKUn7K6iekxocSWK4K5WrtHph_2-WrOXPDyis'
-        };
-        var request =
-            http.MultipartRequest('POST', Uri.parse('$baseUrl/hotel'));
-        request.fields.addAll({
-          'name': hotel.name,
-          'city': hotel.city,
-          'informUz': hotel.informUz,
-          'informRu': hotel.informRu,
-          'informEn': hotel.informEn,
-          'karta': hotel.karta,
-          'tell': hotel.tell.toString(),
-          'categoryId': '1991edea-7d4a-49fb-b627-79b777cf54ae'
-        });
-
-        // BIR NECHTA RASMLARNI JO'NATISH
-        for (var photoPath in hotel.media) {
-          final mimeTypeData =
-              lookupMimeType(photoPath, headerBytes: [0xFF, 0xD8])?.split('/');
-          //------------------
-          request.files.add(await http.MultipartFile.fromPath(
-            'media',
-            photoPath,
-            filename: photoPath.split('/').last,
-            contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
-          ));
-        }
-
-        //  BITTA RASMNI JO'NATISH
-        //  final mimeTypeData =
-        //   lookupMimeType(filePath, headerBytes: [0xFF, 0xD8])?.split('/');
-        // request.files.add(
-        //   await http.MultipartFile.fromPath(
-        //     'media',
-        //     filePath,
-        //     contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
-        //   ).then(
-        //     (value) {
-        //       print(value.field);
-        //       return value;
-        //     },
-        //   ),
-        // );
-
-        request.headers.addAll(headers);
-
-        http.StreamedResponse response = await request.send();
-
-        if (response.statusCode == 201) {
-          print(await response.stream.bytesToString());
-          return response.stream.bytesToString();
-        } else {
-          print('else error: ' + response.reasonPhrase.toString());
-          return response.reasonPhrase;
-        }
-      } catch (e) {
-        print('catch error: ' + e.toString());
-        return null;
+      // FIXME: BIR NECHTA RASMLARNI JO'NATISH
+      for (var photoPath in hotel.media) {
+        final mimeTypeData =
+            lookupMimeType(photoPath, headerBytes: [0xFF, 0xD8])?.split('/');
+        //------------------
+        request.files.add(await http.MultipartFile.fromPath(
+          'media',
+          photoPath,
+          filename: photoPath.split('/').last,
+          contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
+        ));
       }
+
+      //  FIXME:  BITTA RASMNI JO'NATISH
+      //  final mimeTypeData =
+      //   lookupMimeType(filePath, headerBytes: [0xFF, 0xD8])?.split('/');
+      // request.files.add(
+      //   await http.MultipartFile.fromPath(
+      //     'media',
+      //     filePath,
+      //     contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
+      //   ).then(
+      //     (value) {
+      //       print(value.field);
+      //       return value;
+      //     },
+      //   ),
+      // );
+
+      request.headers.addAll(headers);
+
+      http.StreamedResponse response = await request.send();
+
+      if (response.statusCode == 201) {
+        print(await response.stream.bytesToString());
+        return response.stream.bytesToString();
+      } else {
+        print('else error: ' + response.reasonPhrase.toString());
+        return response.reasonPhrase;
+      }
+    } catch (e) {
+      print('catch error: ' + e.toString());
+      return null;
     }
   }
 
