6173526fbf12ec275b4a10f94d6d9228a991bcaa
Ramazon-dev <9621681@gmail.com>
2022-02-27 12:48:37 +0500

    push

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -39,7 +39,7 @@ class HomeScreen extends StatelessWidget {
       SearchPage(),
       HomeBody(cubit: cubit),
       const CarPage(),
-      token.isNotEmpty ?ProfileAuthPage() : SignInPage(),
+      token.isNotEmpty ? const ProfileAuthPage() : const SignInPage(),
     ];
   }
 
