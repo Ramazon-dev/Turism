a3d3797095025d2e9ce6974f46ce31634d3dd842
BahromjonPolat <itjunior.uz@gmail.com>
2022-03-05 20:21:14 +0500

    BusinessAccountInfo is worked

--- lib/screens/profile/auth_profile_page.dart
+++ lib/screens/profile/auth_profile_page.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
 import 'package:mobileapp/cubit/home_cubit/home_cubit.dart';
+import 'package:mobileapp/models/business_account_model.dart';
+import 'package:mobileapp/models/git_model.dart' as git;
+import 'package:mobileapp/services/business_account_service.dart';
 
 class ProfileAuthPage extends StatefulWidget {
   const ProfileAuthPage({Key? key}) : super(key: key);
@@ -29,16 +32,59 @@ class _ProfileAuthPageState extends State<ProfileAuthPage> {
 
   SingleChildScrollView _buildBody(HomeCubit cubit) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          _setTransform(
-            x: 100.0,
-            child: _buildTextButtonWidget(),
-          ),
-          _setTransform(child: _showChangingPasswordFields(), y: -90.0),
-        ],
+      child: _setTransform(
+        y: 140.0,
+        child: FutureBuilder(
+          future: BusinessAccountService().getServiceList(),
+          builder: (ctx, AsyncSnapshot<BusinessAccountModel> snap) {
+            if (snap.hasData) {
+              BusinessAccountModel model = snap.data!;
+              return Column(
+                children: [
+                  // if git is not null, Git will be shown
+                  model.git != null
+                      ? _myGitListWidget(model)
+                      : const SizedBox(),
+
+                  // if hotel list is not null, hotel list will be shown
+                  model.hotels != null
+                      ? _showHotelList(model)
+                      : const SizedBox(),
+
+                  // if restaurant list is not null, restaurant list will be shown
+                  model.restaurants != null
+                      ? _showRestaurants()
+                      : const SizedBox()
+                ],
+              );
+            } else if (snap.hasError) {
+              Center(child: Text(LocaleKeys.personalInfo.tr()));
+            }
+            return const Center(child: CupertinoActivityIndicator());
+          },
+        ),
       ),
     );
+  }
+
+  ListView _showRestaurants() {
+    return ListView.builder(itemBuilder: (ctx, index) {
+      return Container();
+    });
+  }
+
+  ListView _showHotelList(BusinessAccountModel model) {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: model.hotels!.length,
+        itemBuilder: (ctx, i) {
+          return Container();
+        });
+  }
+
+  MyGitListWidjet _myGitListWidget(BusinessAccountModel model) {
+    return MyGitListWidjet(git: git.Git.fromJson(model.git!.toJson()));
   }
 
   // TextButton
--- lib/services/business_account_service.dart
+++ lib/services/business_account_service.dart
@@ -7,12 +7,20 @@ import 'package:mobileapp/models/business_account_model.dart';
 class BusinessAccountService {
   final String _token = GetStorage().read('token');
   final String _baseUrl = 'https://ucharteam-tourism.herokuapp.com/v1/api/user';
-  Future getServiceList()async {
-    Uri url = Uri.parse(_baseUrl);
-    final Map<String, String> headers = {'token' : _token};
-    var response = await http.get(url, headers: headers);
-    var data = jsonDecode(response.body)['data'];
-    BusinessAccountModel userServicesModel = BusinessAccountModel.fromJson(data);
 
+  Future<BusinessAccountModel> getServiceList() async {
+    try {
+      Uri url = Uri.parse(_baseUrl);
+      final Map<String, String> headers = {'token': _token};
+      var response = await http.get(url, headers: headers);
+      var data = jsonDecode(response.body)['data'];
+      BusinessAccountModel businessAccountModel =
+          BusinessAccountModel.fromJson(data);
+      await GetStorage().write('businessAccount', businessAccountModel.toJson());
+      return businessAccountModel;
+    } catch (err) {
+      print('BusinessAccountService.getServiceList: $err');
+    }
+    return BusinessAccountModel();
   }
 }
