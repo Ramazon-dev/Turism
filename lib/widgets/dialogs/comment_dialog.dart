fe8c31c0d081473ad3af214c6a910179a411aab5
BahromjonPolat <itjunior.uz@gmail.com>
2022-03-20 23:19:07 +0500

    Hotelga comment ulandi

--- lib/widgets/dialogs/comment_dialog.dart
+++ lib/widgets/dialogs/comment_dialog.dart
@@ -4,15 +4,18 @@ import 'package:mobileapp/core/components/exporting_packages.dart';
 import 'package:mobileapp/models/comment.dart';
 import 'package:mobileapp/widgets/tiles/comment_tile.dart';
 
+import '../inputs/add_comment_layout.dart';
+
 class CommentListDialog extends StatelessWidget {
   final Map<String, String> headers;
+
   const CommentListDialog({Key? key, required this.headers}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-
+        const AddCommentLayout(),
         FutureBuilder(
             future: CommentService.getComment(headers),
             builder: (ctx, AsyncSnapshot<List<Comment>?> snap) {
@@ -24,7 +27,8 @@ class CommentListDialog extends StatelessWidget {
                 }
                 return Expanded(
                   child: ListView.builder(
-                    padding: MyEdgeInsets.only(left: 20.0 ,right: 65.0, top: 22.0),
+                    padding:
+                        MyEdgeInsets.only(left: 20.0, right: 65.0, top: 22.0),
                     itemCount: snap.data!.length,
                     itemBuilder: (ctx, i) {
                       Comment comment = snap.data![i];
--- lib/widgets/inputs/add_comment_layout.dart
+++ lib/widgets/inputs/add_comment_layout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobileapp/core/components/exporting_packages.dart';
+import 'package:mobileapp/core/data/image_list.dart';
 
 class AddCommentLayout extends StatelessWidget {
   const AddCommentLayout({Key? key}) : super(key: key);
@@ -9,7 +11,23 @@ class AddCommentLayout extends StatelessWidget {
       height: 73.h,
       child: Row(
         children: [
+          CircleAvatar(
+            radius: 22.w,
+            backgroundColor: AppColors.linear,
+            backgroundImage: const CachedNetworkImageProvider(
+              ImageList.profileBlank,
+            ),
+          ),
+          Expanded(
+            child: TextFormField(
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                hintText: 'Добавьте комментари...'
+              ),
+            ),
+          ),
 
+          IconButton(onPressed: (){}, icon: Icon(Icons.send))
         ],
       ),
     );
