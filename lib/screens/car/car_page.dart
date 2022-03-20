868bc20bce4ec9aae694c19655133a8074603f6c
Ramazon-dev <9621681@gmail.com>
2022-03-20 18:32:48 +0500

    nmadurlaaa

--- lib/screens/car/car_page.dart
+++ lib/screens/car/car_page.dart
@@ -16,44 +16,52 @@ class CarPage extends StatelessWidget {
       child: BlocBuilder<CarCubit, CarState>(
         builder: (context, state) {
           CarCubit cubit = context.watch();
-          return FutureBuilder(
-            future: TransportServisec.getDataFromApi(),
-            builder: (context, AsyncSnapshot<TransportModelsssssss> snap) {
-              if (snap.hasError) {
-                return const Center(
-                  child: Text("Internet conntection has error"),
-                );
-              } else if (snap.hasData) {
-                var data = snap.data;
-                debugPrint('data keldi mana ${snap.data!}');
-                return GridView.builder(
-                  padding: MyEdgeInsets.symmetric(h: 16.0, v: 25.0),
-                  itemCount: data!.data!.length,
-                  itemBuilder: (ctx, i) {
-                    debugPrint("rasm url qismi ${data.data![i].media}");
-
-                    return CarInfoCard(
-                      // car: MockData.carModel,
-                      carImage: data.data![i].media![0],
-                      carName: data.data![i].name!,
-                      carPrice: data.data![i].price!,
-                      carInfo: data.data![i].informUz!,
-                      carNumber: data.data![i].tell!,
-                      rating: data.data![i].reyting!,
-                      users: data.data![i].users!,
+          return Column(
+            children: [
+              CityListWidget(
+                onCityChanged: cubit.onCityChanged,
+                cityName: cubit.city.name,
+              ),
+              FutureBuilder(
+                future: TransportServisec.getDataFromApi(),
+                builder: (context, AsyncSnapshot<TransportModelsssssss> snap) {
+                  if (snap.hasError) {
+                    return const Center(
+                      child: Text("Internet conntection has error"),
                     );
-                  },
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisExtent: 230.h,
-                    crossAxisSpacing: getWidth(8.0),
-                    mainAxisSpacing: getHeight(20.0),
-                  ),
-                );
-              } else {
-                return const CircularProgressIndicator();
-              }
-            },
+                  } else if (snap.hasData) {
+                    var data = snap.data;
+                    debugPrint('data keldi mana ${snap.data!}');
+                    return GridView.builder(
+                      padding: MyEdgeInsets.symmetric(h: 16.0, v: 25.0),
+                      itemCount: data!.data!.length,
+                      itemBuilder: (ctx, i) {
+                        debugPrint("rasm url qismi ${data.data![i].media}");
+
+                        return CarInfoCard(
+                          // car: MockData.carModel,
+                          carImage: data.data![i].media![0],
+                          carName: data.data![i].name!,
+                          carPrice: data.data![i].price!,
+                          carInfo: data.data![i].informUz!,
+                          carNumber: data.data![i].tell!,
+                          rating: data.data![i].reyting!,
+                          users: data.data![i].users!,
+                        );
+                      },
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisExtent: 230.h,
+                        crossAxisSpacing: getWidth(8.0),
+                        mainAxisSpacing: getHeight(20.0),
+                      ),
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ],
           );
         },
       ),
