5bc3a9a1d92bdd4c631512bbfa78e62f605ff002
BahromjonPolat <itjunior.uz@gmail.com>
2022-03-27 22:05:57 +0500

    Choose language page is ready

--- lib/screens/on_boarding/initial_page.dart
+++ lib/screens/on_boarding/initial_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/core/components/exporting_packages.dart';
 import 'package:mobileapp/screens/on_boarding/on_boarding_page.dart';
+import 'package:mobileapp/screens/settings/choose_language_page.dart';
 import 'package:mobileapp/translations/locale_keys.g.dart';
 import 'package:mobileapp/widgets/lets_go.dart';
 
@@ -15,12 +16,12 @@ class InitialPage extends StatelessWidget {
         decoration: MyDecoration.assetImage(assetImage: AppImages.initial),
         child: Column(
           children: [
-            const LetsGo(text: 'Добро пожаловать!', color: AppColors.black),
+            LetsGo(text: LocaleKeys.welcome.tr(), color: AppColors.black),
             const Spacer(),
             OutlinedButton(
               onPressed: () {
                 Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => OnBoardingPage()));
+                    MaterialPageRoute(builder: (_) => const ChooseLanguagePage()));
               },
               child: Text(
                 LocaleKeys.start.tr(),
